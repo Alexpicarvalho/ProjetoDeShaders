5020989ceca5b5938965ccffe30d70b6aaa9b3b3
Alexpicarvalho <a21094@alunos.ipca.pt>
2023-06-13 00:50:35 +0100

    battery life

--- Assets/Script/FlashLight/PositionInWorldToShader.cs
+++ Assets/Script/FlashLight/PositionInWorldToShader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class PositionInWorldToShader : MonoBehaviour
 {
@@ -11,8 +12,31 @@ public class PositionInWorldToShader : MonoBehaviour
     Ray ray;
     float _distance;
 
+    [Header("Lantern things")]
+    public float _maxCharge = 100f;
+    public float _currentCharge = 50f;
+    public float _chargeOnPickUp = 10f;
+    public float _chargeLossPerSecond = 10f;
+    public bool _lanternOn = false;
+
+    [Header("UI")]
+    public TextMeshProUGUI _batteryText;
+
+
     private void Update()
     {
+        //Battery Handeling
+        if(_lanternOn) _currentCharge -= _chargeLossPerSecond * Time.deltaTime;
+
+        _batteryText.text = ((int)_currentCharge).ToString() + " / " + _maxCharge.ToString();
+
+        if(Input.GetButtonDown("Fire1") && !_lanternOn) _lanternOn = true;
+        else if(Input.GetButtonDown("Fire1") && _lanternOn) _lanternOn = false;
+
+        if (_currentCharge <= 0) _lanternOn = false;
+
+        if (_lanternOn) return;
+
         // Obtém a posição do ponto para o qual o objeto está apontando
         Vector3 pointedPosition = GetPointedPosition();
 
@@ -38,6 +62,16 @@ public class PositionInWorldToShader : MonoBehaviour
 
         // Caso contrário, retorna uma posição arbitrária (por exemplo, a posição do mouse no plano XY)
         return new Vector3(mousePosition.x, mousePosition.y, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(_currentCharge == _maxCharge || !other.CompareTag("Battery")) return;
+
+        Destroy(other.gameObject);
+
+        _currentCharge += _chargeOnPickUp;
+        if (_currentCharge >= _maxCharge) _currentCharge = _maxCharge;
     }
 
     private void OnDrawGizmos()
